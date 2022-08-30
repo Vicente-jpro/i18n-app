549bf00b7fe7a1a8c29ecf16d0a8717108942679
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-08-30 11:50:07 +0100

    Nav bar added

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_170458) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
