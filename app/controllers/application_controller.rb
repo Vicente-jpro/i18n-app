a76f772fdb1415b5aa587727e5f52983919a67c5
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-09-03 13:51:24 +0100

    Space added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	around_action :switch_locale
-
+	
 	def switch_locale(&action)
 	  locale = params[:locale] || I18n.default_locale
 	  I18n.with_locale(locale, &action)
